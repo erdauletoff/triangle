bab2967502840a51680d8a0b5c959a56abeb3e29
Darkhan <darkhan_erdawletov@student.itpu.uz>
2023-12-08 15:17:16 +0500

    done!

--- src/main/java/com/epam/rd/autotasks/triangle/Triangle.java
+++ src/main/java/com/epam/rd/autotasks/triangle/Triangle.java
@@ -2,16 +2,38 @@ package com.epam.rd.autotasks.triangle;
 
 class Triangle {
 
+    final Point a;
+    final Point b;
+    final Point c;
+    final double ab;
+    final double bc;
+    final double ac;
+
     public Triangle(Point a, Point b, Point c) {
-        //TODO
+        this.a = a;
+        this.b = b;
+        this.c = c;
+
+        ab = findDistance(a, b);
+        bc = findDistance(b, c);
+        ac = findDistance(a, c);
+
+        if (!(ab + bc > ac && ac + bc > ab && ab + ac > bc)) {
+            throw new IllegalArgumentException();
+        }
     }
 
     public double area() {
-        //TODO
+        double s = (ab + bc + ac) / 2;
+        return Math.sqrt(s * (s - ab) * (s - bc) * (s - ac));
     }
 
     public Point centroid(){
-        //TODO
+        return new Point((a.getX() + b.getX() + c.getX()) / 3, (a.getY() + b.getY() + c.getY()) / 3);
+    }
+
+    private double findDistance(Point first, Point second) {
+        return Math.sqrt(Math.pow(second.getX() - first.getX(), 2) + Math.pow(second.getY() - first.getY(), 2));
     }
 
 }
